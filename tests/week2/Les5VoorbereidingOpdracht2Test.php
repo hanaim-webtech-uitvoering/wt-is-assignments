d7ba26a619e6d4b19881d1b7af32f9ada416f28c
Fritz van Deventer <fritzvd@users.noreply.github.com>
2021-09-24 12:30:27 +0200

    skip les 5 voorbereiding 2

--- tests/week2/Les5VoorbereidingOpdracht2Test.php
+++ tests/week2/Les5VoorbereidingOpdracht2Test.php
@@ -6,7 +6,7 @@ require('src/week2/Les5VoorbereidingOpdracht2_film_naar_minuten.php');
 final class Les5VoorbereidingOpdracht2Test extends TestCase
 {
     function setUp():void {
-        // $this->markTestSkipped('Nog niet bezig met deze les.');
+        $this->markTestSkipped('Nog niet bezig met deze les.');
     }
     
     function testFilmNaarMinuten() {
